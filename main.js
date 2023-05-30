b6c84f75277a2fab635d6a5cd68a18bb9efcf7b5
Farrux-react <farruxusarbayev@gmail.com>
2023-05-30 04:04:35 +0300

    js homework 4

---
+++ main.js
@@ -0,0 +1,160 @@
+//1 - masala
+/*
+function x(a, b) {
+    return a - b;
+}
+
+console.log(x(5, 3));
+*/
+
+//2 - masala
+/*
+function x(a, b) {
+    return a[b];
+}
+
+console.log(x("salom", 0));
+*/
+
+//3 - masala
+/*
+function x(a) {
+
+    if (a[0] == "h") {
+        if (a[1] == "i") {
+            return true;
+        }
+    } else {
+        return false;
+    }
+
+}
+
+console.log(x("hi salom"));
+*/
+
+//4 - masala
+/*
+function reverseWord(a) {
+    let b = "";
+    for (let i = a.length - 1; i >= 0; i--) {
+        b += a[i];
+    }
+    return b;
+}
+
+console.log(reverseWord("salom"));
+*/
+
+//5 - masala
+/*
+function x(a) {
+
+    if (a % 3 == 0 || a % 5 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log(x(15));
+*/
+
+//6 - masala
+/*
+function x(a, b) {
+
+    if (a > 0 && a <= 100 && b > 0 && b <= 100) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log(x(25, 55));
+*/
+
+//7 - masala
+/*
+function x(a, b) {
+
+    if (a > b) {
+        return "1 katta";
+    }
+
+    if (a < b) {
+        return "2 katta";
+    }
+
+    if (a == b) {
+        return "Teng";
+    }
+
+}
+
+console.log(x(5, 5));
+*/
+
+//8 - masala
+/*
+function x(a, b, c) {
+
+    c = a + b;
+
+    if (c >= 30 && c <= 50) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log(x(40, 10));
+*/
+
+//9 - masala
+/*
+function x(a, b) {
+
+    if (a % b == 10) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log(x(110, 20));
+*/
+
+//10 - masala
+/*
+function x(a, b) {
+
+    if (a == b) {
+        return (a + b) * 2;
+    } else {
+        return a + b;
+    }
+
+}
+
+console.log(x(20, 20));
+*/
+
+//11 - masala
+/*
+function x(a, b) {
+
+    if (a == 10 || b == 10 || a + b == 10) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log(x(10, 5));
+*/
